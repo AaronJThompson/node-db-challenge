2a425cd313c66fb15b647f124c2447e2540c21e3
AaronJThompson <aaronjthompson98@gmail.com>
2019-07-19 09:41:21 +0100

    POST for actions

--- projects/projects-router.js
+++ projects/projects-router.js
@@ -39,6 +39,27 @@ function validateNewProject(req, res, next) {
     }
 }
 
+function validateNewAction(req, res, next) {
+    const { notes, description } = req.body;
+    const errors = [];
+    try {
+        if (description) {
+            if (description.length > 128) errors.push({ error: "Description field limit is 128 characters" });
+            if (notes && notes.length > 128) errors.push({ error: "Notes field limit is 128 characters" });
+            if (errors.length === 0) {
+                req.action = { notes, description };
+                next();
+            } else {
+                res.status(400).json(errors);
+            }
+        } else {
+            throw new Error('Description is required');
+        }
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+}
+
 router.get('/', async (req, res) => {
     try {
         const projects = ProjectsDB.find();
@@ -48,7 +69,7 @@ router.get('/', async (req, res) => {
     }
 });
 
-router.get('/:id', validateProject, async(req, res) => {
+router.get('/:id', validateProject, async (req, res) => {
     try {
         const proj = await ProjectsDB.getProjectWithActions(req.project.id);
         res.status(200).json(proj);
@@ -63,5 +84,14 @@ router.post('/', validateNewProject, async (req, res) => {
         res.status(201).json(proj);
     } catch (error) {
         res.status(500).json({ error: "Couldn't add project" });
+    }
+})
+
+router.post('/:id', validateProject, validateNewAction, async (req, res) => {
+    try {
+        const act = await ProjectsDB.insertAction(req.action);
+        res.status(201).json(act);
+    } catch (error) {
+        res.status(500).json({ error: "Couldn't create action" });
     }
 })
