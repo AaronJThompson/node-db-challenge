84cce4e05d8ce81e5852e577439a88f9da791f90
AaronJThompson <aaronjthompson98@gmail.com>
2019-07-19 10:55:26 +0100

    Add actions CRUD

--- actions/actions-router.js
+++ actions/actions-router.js
@@ -10,7 +10,7 @@ async function validateAction(req, res, next) {
         try {
             const act = await ProjectsDB.findAction(req.params.id);
             if (!act) throw new Error('Action null');
-            req.actiion = act;
+            req.action = act;
             next();
         } catch (error) {
             res.status(404).json({ error: "Action id not valid" });
@@ -20,8 +20,31 @@ async function validateAction(req, res, next) {
         next();
     }
 }
-router.update('/:id', validateAction, validators.validateNewAction, async (req,res) => {
-    
+
+router.put('/:id', validateAction, validators.validateNewAction, async (req,res) => {
+    try {
+        const act = await ProjectsDB.updateAction(req.action, req.params.id);
+        res.status(201).json(act);
+    } catch (error) {
+        res.status(500).json({ error: "Couldn't update action" });
+    }
+})
+
+router.delete('/:id', validateAction, async (req,res) => {
+    try {
+        const act = await ProjectsDB.removeAction(req.action.id);
+        res.status(200).json(act);
+    } catch (error) {
+        res.status(500).json({ error: "Couldn't delete action" });
+    }
+})
+
+router.get('/:id', validateAction, async (req, res) => {
+    try {
+        res.status(200).json(req.action);
+    } catch (error) {
+        res.status(500).json({ error: "Couldn't get action" });
+    }
 })
 
 module.exports = router;
