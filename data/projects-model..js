d76ef58db60c239f995b98c74e5397f284a87425
AaronJThompson <aaronjthompson98@gmail.com>
2019-07-19 09:02:30 +0100

    CRUD operations for actions

--- data/projects-model..js
+++ data/projects-model..js
@@ -35,3 +35,31 @@ function update(fields, id) {
         .update(fields)
         .then(() => findById(id));
 }
+
+function findAction(id) {
+    return db('actions')
+        .where({ id: Number(id) })
+        .then(ids => ids[0]);
+}
+
+function insertAction(action, id) {
+    const actionWithProj = {...action, project_id: Number(id)};
+    return db('actions')
+        .insert(actionWithProj)
+        .then(ids => findAction(ids[0]));
+}
+
+function updateAction(fields, id) {
+    return db('actions')
+        .where({ id: Number(id) })
+        .update(fields)
+        .then(() => findAction(id));
+}
+
+function removeAction(id) {
+    const act = await findAction(id);
+    return db('actions')
+        .where({ id: Number(id) })
+        .del()
+        .then(() => act);
+}
